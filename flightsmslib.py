46ed225f9fbf274090506f9811bdfa009f311a3e
HarravanS <augusto@MacBook-Pro-di-Augusto.local>
2010-02-26 20:07:28 +0100

    modified:   flightsmslib.py

--- flightsmslib.py
+++ flightsmslib.py
@@ -11,7 +11,7 @@ import sqlite3.dbapi2 as sqlite
 #configuration variables
 gateway = "https://sms.vola.it/cgi/volasms_gw_plus2.php"
 serial = "TR45GDLBO730HDUIEQJ5"
-db_name = "volasms.db"
+db_name = "flightsms.db"
 
 
 #get user and pas
