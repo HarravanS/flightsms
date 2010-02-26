36aa2b6977d839bffbb5c9fa1835510d40e1d924
HarravanS <augusto@MacBook-Pro-di-Augusto.local>
2010-02-26 21:53:10 +0100

    messages fix

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ messaggi = vola_cmd()
 def main_menu():
 	mchoise = "CHOISE AN OPTION\n\n\
 	x) press X for settings\n\
-	m) press M to send messages\n\
+	m) press M to send message\n\
 	c) press C to view credit\n\
 	Q) press Q to exit"
 	print mchoise
@@ -26,11 +26,11 @@ def main_menu():
 	elif m_action == "Q":
 		quit("\n\nbye\n\n")
 	else:
-		print "\n\nYou must select one of following option\n\n"
+		print "\n\nYou must select one of the following options\n\n"
 		main_menu()
 
 def settings_menu():
-	schoise = "CHOISE AN OPTIONS\n\n\
+	schoise = "CHOISE AN OPTION\n\n\
 	a) press A for view settings\n\
 	u) press U to set new user and pass\n\
 	k) press K to set receive key\n\
@@ -58,11 +58,11 @@ def settings_menu():
 		m_action,s_action = "",""
 		main_menu()
 	else:
-		print "\n\nYou must select one of following option\n\n"
+		print "\n\nYou must select one of the following options\n\n"
 		settings_menu()
 
 def msg_menu():
-	gchoise = "CHOISE AN OPTIONS\n\n\
+	gchoise = "CHOISE AN OPTION\n\n\
 	N) press N for view settings\n\
 	q) return to main menu"
 	print gchoise
