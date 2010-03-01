26edf5c34dc9261697e57cfd12e243253ca29ca4
HarravanS <augusto@MacBook-Pro-di-Augusto.local>
2010-03-01 21:32:31 +0100

    todo & history fix

--- history.py
+++ history.py
@@ -7,6 +7,7 @@ from conf import *
 
 class smshistory():
 
+	#new history entry
 	def add_History(self, cid, receiver, sender, message, genre, status, orderid, notes):
 		connect = sqlite.connect(db_name)
 		dbcur=connect.cursor()
@@ -17,6 +18,7 @@ class smshistory():
 	def get_History(self,args):
 		pass
 	
+	#return all history
 	def show_History(self):
 		connect = sqlite.connect(db_name)
 		dbcur=connect.cursor()
@@ -28,6 +30,7 @@ class smshistory():
 		dbcur.close()
 		connect.close()
 	
+	#delete all history entry
 	def clear_History(self):
 		connect = sqlite.connect(db_name)
 		dbcur=connect.cursor()
@@ -35,10 +38,15 @@ class smshistory():
 		dbcur.close()
 		connect.close()
 	
+	#obtain last cid in history
 	def lastcid_History(self):
 		connect = sqlite.connect(db_name)
 		dbcur=connect.cursor()
-		dbcur.execute("select * from history where cid")
+		dbcur.execute("select * from history order by cid desc")
+
+		self.cid_row = dbcur.fetchone()
+		return self.cid_row[1]
+
 		dbcur.close()
 		connect.close()
 
